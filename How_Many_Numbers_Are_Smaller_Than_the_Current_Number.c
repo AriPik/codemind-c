61de34136cb07bca4dd581c535eefa11dd64a5b3
AriPik <97512141+AriPik@users.noreply.github.com>
2022-02-22 20:37:37 +0530

    #include<stdio.h> int main() {     int arr[20],i,j,n,count=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         count=0;         for(j=0;j<n;j++)         {             if(arr[j]<arr[i] && i!=j)             {                 count++;             }         }         printf("%d ",count);     }     return 0; }

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int arr[20],i,j,n,count=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        count=0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[j]<arr[i] && i!=j)
+            {
+                count++;
+            }
+        }
+        printf("%d ",count);
+    }
+    return 0;
+}
