398e3c4b05aff950ae07845a66cb6e2a36814c62
Arijit Das <97512141+AriPik@users.noreply.github.com>
2022-06-01 11:14:10 +0530

    #include<stdio.h> #include<math.h> int main() {     int n,a;     float b;     scanf("%d",&n);     a=sqrt(n);     b=sqrt(n);     if(b-a==0)     {         printf("True");     }     else     {         printf("False");     }     return 0; }

---
+++ Square_Number.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n,a;
+    float b;
+    scanf("%d",&n);
+    a=sqrt(n);
+    b=sqrt(n);
+    if(b-a==0)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+    return 0;
+}
